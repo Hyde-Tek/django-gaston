c9d1684523020eb9159854361654743783664887
Georgios A. Tsakalos <ozmax.planet@gmail.com>
2020-01-25 15:50:12 +0200

    add setup parameter that includes template files in sdist
    release 0.0.6

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='django-gaston',
-    version='0.0.5',
+    version='0.0.6',
     author='Hyde Tek',
     description='A django menu generator',
     long_description=long_description,
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
+    include_package_data=True,
 )
