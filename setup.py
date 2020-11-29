4d8735862454968070b80ee8afc14823a8e409d4
Nick Chrysochoidis <nxriso@hotmail.com>
2020-11-29 22:08:44 +0200

    release 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='django-gaston',
-    version='0.2.0',
+    version='0.3.0',
     url='https://github.com/Hyde-Tek/django-gaston/',
     author='Hyde Tek',
     description='A django menu generator',
