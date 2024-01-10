936d5193c0c7438670de73fbd6838ec73b174b26
Georgios A. Tsakalos <george@dimersoftware.com>
2024-01-10 12:46:56 +0200

    release 0.4.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='django-gaston',
-    version='0.4.0',
+    version='0.4.1',
     url='https://github.com/Hyde-Tek/django-gaston/',
     author='Hyde Tek',
     description='A django menu generator',
